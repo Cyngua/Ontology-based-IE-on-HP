fc04c9ef5784d87ffe097b55e0d5962ed2b72724
Cyngua <cynthia-cai@outlook.com>
2025-03-27 00:56:02 -0400

    add: bio transform

--- prepare/annotation_to_bio.py
+++ prepare/annotation_to_bio.py
@@ -3,11 +3,11 @@ Character span annotation into BIO format
 '''
 import pandas as pd
 import spacy
-from spacy.tokens import Doc
 import tqdm
 
 # spacy nlp pipeline
 nlp = spacy.blank("en")
+nlp.add_pipe('sentencizer')
 
 # load the semantic group map
 semantic_group_map = pd.read_csv('prepare/semantic_group_map.csv')
@@ -22,7 +22,7 @@ annotations_df = pd.read_csv('data/benchmark_data/train_annotations.csv')
 def char_spans_to_bio(note_id):
     '''
     Given a single note id convert the span to bio
-    Returns a list of (token.text, label) tuples.
+    Returns a list of list (token.text, label) tuples.
     '''
 
     text_row = notes_df[notes_df['note_id'] == note_id]
@@ -30,7 +30,7 @@ def char_spans_to_bio(note_id):
         return []
 
     text = text_row['text'].iloc[0]
-    doc = nlp(text)
+    doc = nlp(text.replace('\n', ' '))
     labels = ["O"] * len(doc)
 
     # extract the annotations spans according to note id
@@ -39,17 +39,37 @@ def char_spans_to_bio(note_id):
         start_idx = row["start"]
         end_idx = row["end"]
         concept_id = row["concept_id"]
+        span = doc.char_span(start_idx, end_idx)
+        if span:
+            group = semantic_dict.get(concept_id, "UNK")  # fallback if missing
+            labels[span.start] = f"B-{group}"
+            labels[span.start+1: span.end] = [f"I-{group}"]*(span.end-span.start-1)
 
-        group = semantic_dict.get(concept_id, "UNK")  # fallback if missing
+    # Split token-labels by sentence
+    sentence_bio_pairs = []
+    for sent in doc.sents:
+        sent_tokens = [token.text for token in sent]
+        sent_labels = labels[sent.start : sent.end]
+        sentence_bio_pairs.append(list(zip(sent_tokens, sent_labels)))
 
-        labels[start_idx] = f"B-{group}"
-        if end_idx > start_idx:
-            labels[start_idx+1: end_idx+1] = f"I-{group}"
-
-    return list(zip([token.text for token in doc], labels))
+    return sentence_bio_pairs
 
 def main():
-    pass
+    rows = []  # list to collect output rows
+    note_id_list = notes_df['note_id'].unique()
+    for note_id in tqdm.tqdm(note_id_list):
+        sentence_bio_pairs = char_spans_to_bio(note_id)
+        for sentence_id, token_label_pairs in enumerate(sentence_bio_pairs):
+            tokens = [token for token, _ in token_label_pairs]
+            labels = [label for _, label in token_label_pairs]
+            row_entries = [note_id, sentence_id, ' '.join(tokens), ','.join(labels)]
+            rows.append(row_entries)
+    df = pd.DataFrame(rows, columns=["note_id", "sentence_id", "sentence", "tags"])
+    print(df.head())
+    df.to_csv('processed_notes.csv', index=None)
+
+def test():
+    print(char_spans_to_bio('10097089-DS-8')[60])
 
 if __name__ == "__main__":
     main()
